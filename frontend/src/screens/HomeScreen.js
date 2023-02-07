c869334907035bfaa52201d9cde0e0e9a2f042ce
Shivam Gupta <gupta.shivam2604@gmail.com>
2023-02-08 02:19:11 +0530

    Started project

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 const HomeScreen = () => {
+
+  const userLogin = useSelector((state) => state.userLogin)
+  const { loading, error, userInfo } = userLogin
+
+
+  let navigate = useNavigate();
+  
+  useEffect(() => {
+
+    if (!userInfo) {
+      navigate('/login')
+    }
+  }, [navigate, userInfo])
+
+
   return (
     <div>HomeScreen</div>
   )
